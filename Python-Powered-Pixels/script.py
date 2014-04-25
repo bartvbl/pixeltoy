6ea7e9c32996ff726814179c4ac907443d140401
Bart van Blokland <bartvblokl@gmail.com>
2014-04-25 14:06:20 +0200

    Some visual improvements on the sample script

--- Python-Powered-Pixels/script.py
+++ Python-Powered-Pixels/script.py
@@ -12,13 +12,18 @@ class Ball:
 	
 	def draw(self):
 		self.ySpeed -= 1
+		self.x += self.xSpeed
 		self.y += self.ySpeed
 		useColour(self.red, self.green, self.blue, 255)
 		drawCircle(self.x, self.y, self.radius)
 		
-		if((self.y < 0) & (self.ySpeed < 0)):
+		if self.x - self.radius > _screenWidth:
+			self.x -= _screenWidth + 2 * self.radius
+		if self.x + self.radius < 0:
+			self.x += _screenWidth + 2 * self.radius
+		if((self.y - self.radius < 0) & (self.ySpeed < 0)):
 			self.ySpeed *= -0.95
-			self.y = 0
+			self.y = self.radius
 			self.radius -= 3
 			if self.radius < 0:
 				self.generateBall()
@@ -26,6 +31,8 @@ class Ball:
 	def generateBall(self):
 		self.x = random() * _screenWidth
 		self.y = random() * _screenHeight
+		self.ySpeed = 0
+		self.xSpeed = (random() - 0.5) * 15.0
 		self.radius = (random() * 70) + 10
 		self.red = (random() * 254) + 1
 		self.green = (random() * 254) + 1
@@ -35,7 +42,7 @@ ballArray = []
 for i in xrange(0, 100):
 	ballArray.append(Ball())
 
-while 1:
+while True:
 	newFrame()
 	for ball in ballArray:
 		ball.draw()
